189a822631c4b4fe96948bb9be932d31e17a9a42
miyanana <60533284+miyazakinana@users.noreply.github.com>
2023-11-16 05:23:10 +0000

    Update page.tsx 美味しかったです

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>Hello world</div>
-     )
-    
+  return <div>Hello world.今日のお昼ご飯納豆ご飯</div>;
 }
